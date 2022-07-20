0fdb0107743f60c38a199a691e16268449449841
DilanTharaka <tharakad049@gmail.com>
2022-07-21 02:37:09 +0530

    update login form

--- front-end/src/pages/Session/Login/index.jsx
+++ front-end/src/pages/Session/Login/index.jsx
@@ -1,68 +1,31 @@
 import React, {Component} from "react";
-import "./style.css";
+import "../../Session/Login/style.css";
 
 class Login extends Component {
-
     constructor(props) {
         super(props);
     }
 
     render() {
-        const signUpButton = document.getElementById('signUp');
-        const signInButton = document.getElementById('signIn');
-        const container = document.getElementById('container');
-        signUpButton.addEventListener('click', () => {
-            container.classList.add("right-panel-active");
-        });
-        signInButton.addEventListener('click', () => {
-            container.classList.remove("right-panel-active");
-        });
         return (
             <header>
-                <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
-                <div className="container" id="container">
-                    <div className="form-container sign-up-container">
-                        <form action="#">
-                            <h1>Create Account</h1>
-                            <div className="social-container">
-                                <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                                <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                                <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                            </div>
-                            <span>or use your email for registration</span>
-                            <input type="text" placeholder="Name"/>
-                            <input type="email" placeholder="Email"/>
-                            <input type="password" placeholder="Password"/>
-                            <button>Sign Up</button>
-                        </form>
+                <div id="loginform"><h2 id="headerTitle">Login</h2>
+                    <div>
+                        <div className="row">
+                            <label>Username</label>
+                            <input type="text" placeholder="Enter your username"/></div>
+                        <div className="row">
+                            <label>Password</label>
+                            <input type="password" placeholder="Enter your password"/></div>
+                        <div id="button" className="row">
+                            <button>Log in</button>
+                        </div>
                     </div>
-                    <div className="form-container sign-in-container">
-                        <form action="#">
-                            <h1>Sign in</h1>
-                            <div className="social-container">
-                                <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                                <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                                <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                            </div>
-                            <span>or use your account</span>
-                            <input type="email" placeholder="Email"/>
-                            <input type="password" placeholder="Password"/>
-                            <a href="#">Forgot your password?</a>
-                            <button>Sign In</button>
-                        </form>
-                    </div>
-                    <div className="overlay-container">
-                        <div className="overlay">
-                            <div className="overlay-panel overlay-left">
-                                <h1>Welcome Back!</h1>
-                                <p>To keep connected with us please login with your personal info</p>
-                                <button className="ghost" id="signIn">Sign In</button>
-                            </div>
-                            <div className="overlay-panel overlay-right">
-                                <h1>Hello, Friend!</h1>
-                                <p>Enter your personal details and start journey with us</p>
-                                <button className="ghost" id="signUp">Sign Up</button>
-                            </div>
+                    <div id="alternativeLogin">
+                        <label>Or sign in with:</label>
+                        <div id="iconGroup"><a href="#" id="facebookIcon">
+                        </a><a href="#" id="twitterIcon"></a><a
+                            href="#" id="googleIcon"></a>
                         </div>
                     </div>
                 </div>
