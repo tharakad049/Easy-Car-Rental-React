b029080a4c8c1580f535aa22db0394aac80e010f
DilanTharaka <tharakad049@gmail.com>
2022-07-22 13:30:39 +0530

    add rental request form

--- front-end/src/pages/Layouts/Layout.jsx
+++ front-end/src/pages/Layouts/Layout.jsx
@@ -5,11 +5,12 @@ import Register from "../Session/Register/register";
 import AdminNavBar from "../../components/admin/NavBar";
 import ManageCar from "../Admin/ManageCar/ManageCar";
 import CustomerNavBar from "../../components/Customer/NavBar";
+import RentalRequest from "../Admin/RentalRequest/RentalRequest";
 
 const Layout = () => {
   return (
     <Fragment>
-      {/*<HomePage />*/}   <ManageCar/>
+      {/*<HomePage />*/}   <RentalRequest/>
 {/*     <Login/>*/}{/* <AdminNavBar/>*/}
 {/*   <Register/>*/}
     </Fragment>
